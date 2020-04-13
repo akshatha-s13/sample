ba0be5b506aa0094a35aa7c385a2b3cc4b684c6d
Akshatha S <30971444+akshatha-s13@users.noreply.github.com>
2020-04-13 16:44:57 +0530

    Update conf.py

--- sample-docs/source/conf.py
+++ sample-docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('../..'))
 
 
@@ -27,7 +28,7 @@ author = 'Author'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ 'm2r', 'sphinx.ext.autodoc'
+extensions = [ 'm2r', 'sphinx.ext.autodoc','sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,17 +45,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
-
-html_context = {                                                             
-        'css_files': [                                                           
-            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',            
-            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',       
-            '_static/theme_overrides.css',                                       
-        ],                                                                       
-    }
+html_static_path = ['_static']
